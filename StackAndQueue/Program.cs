05e96df49e92801a449c2ff2ae81bdbe42c00db3
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-18 00:50:59 +0530

    [Hemant] Added UC1

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace StackAndQueue
 {
@@ -7,6 +8,40 @@ namespace StackAndQueue
         public static void Main()
         {
             Console.WriteLine("Welcome to Data Structure Stack And Queue Problems");
+
+            bool check = true;
+
+            Start:
+            while (check)
+            {
+                
+                Console.WriteLine("Please Select Options :\n" +
+                    "1)Elements added using Linked List in Stack using Push method\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        StackLinkedList stack = new StackLinkedList();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Pushing Elements in the Stack as :\n");
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Element in stack given as :\n" );
+                        stack.Display();
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter Proper Option");
+                        break;
+                        goto Start;
+                }
+            }
+
+
         }
     }
 }
---
+++ StackAndQueue/StackLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class StackLinkedList
+    {
+        Node top;
+        
+
+        public StackLinkedList()
+        {
+            top = null;
+        }
+        public void Push(int data)
+        {
+            Node nodeObj = new Node(data);
+            nodeObj.Next = top;
+            top = nodeObj;
+            Console.WriteLine("{0} Push to the stack", data);
+        }   
+        public void Display()
+        {
+            if (top == null)
+                Console.WriteLine("Stack is Empty here");
+
+            Node temp=top;
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data +" ");
+                temp=temp.Next;
+            }
+        }
+    }
+}
