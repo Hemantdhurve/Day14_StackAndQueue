2c20c58dd11cac66ea6f02e283e07446d7e87c3d
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-17 23:51:40 +0530

    Welcome Greeting Message

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Data Structure Stack And Queue Problems");
+        }
+    }
+}
