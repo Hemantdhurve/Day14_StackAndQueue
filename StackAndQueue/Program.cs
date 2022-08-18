e580322acb08f89c711f70e4a0c8ec3e8773ecd5
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-19 00:17:04 +0530

    [Hemant] Added UC2 Peek,Pop and Empty method in Stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -16,7 +16,8 @@ namespace StackAndQueue
             {
                 
                 Console.WriteLine("Please Select Options :\n" +
-                    "1)Elements added using Linked List in Stack using Push method\n");
+                    "1)Elements added using Linked List in Stack using Push method\n" +
+                    "2)Element deleted from stack using Pop method\n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
 
@@ -34,14 +35,43 @@ namespace StackAndQueue
                         stack.Display();
                         Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
                         break;
+
+                        
+                    case 2:
+                        StackLinkedList stack1 = new StackLinkedList();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Pushing Elements in the Stack as :\n");
+                        stack1.Push(70);
+                        stack1.Push(30);
+                        stack1.Push(56);
+
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Element in stack given as :\n");
+
+                        stack1.Display();
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+
+                        stack1.Peek();
+                        stack1.Display();
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+                        stack1.pop();
+                        stack1.Display();
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+
+                        stack1.IsEmpty();
+                        stack1.Display();
+                        Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+
+                        Console.WriteLine("All elements are popped out from the stack");
+                        break;
+
+
                     default:
                         Console.WriteLine("Please Enter Proper Option");
                         break;
                         goto Start;
                 }
             }
-
-
         }
     }
 }
