0eeac12413035502fae28431d6c016ecbc54fad5
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-19 01:08:06 +0530

    [Hemant] Added UC3 Adding Elements using Enqueue Method

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -17,7 +17,8 @@ namespace StackAndQueue
                 
                 Console.WriteLine("Please Select Options :\n" +
                     "1)Elements added using Linked List in Stack using Push method\n" +
-                    "2)Element deleted from stack using Pop method\n");
+                    "2)Element deleted from stack using Pop method\n" +
+                    "3)Elements added using Linked List in Queue usin Enqueue method\n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
 
@@ -65,6 +66,19 @@ namespace StackAndQueue
                         Console.WriteLine("All elements are popped out from the stack");
                         break;
 
+                    case 3:
+
+                        QueuelinkedList queue = new QueuelinkedList();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Adding elements in the Queue :");
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);                       
+                        queue.Display();
+                        Console.WriteLine("\n");
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+
+                        break;
 
                     default:
                         Console.WriteLine("Please Enter Proper Option");
